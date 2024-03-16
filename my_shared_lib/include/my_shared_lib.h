9c5e371468827c75f758cc91c4d7a451a80a246e
Yoshiki Matsuda <y_matsuda@fixstars.com>
2024-03-16 18:44:14 +0900

    🐛 Fix build on Windows

--- my_shared_lib/include/my_shared_lib.h
+++ my_shared_lib/include/my_shared_lib.h
@@ -1,4 +1,16 @@
 #pragma once
 #include <string>
 
-__attribute__((visibility("default"))) void SharedLibFunc(const std::string& s);
+#ifdef _MSC_VER  // Windows
+#ifdef MY_SHARED_LIB_BUILD_DLL
+#define DLL_EXPORT __declspec(dllexport)
+#else
+#define DLL_EXPORT __declspec(dllimport)
+#endif
+#else
+#define DLL_EXPORT __attribute__((visibility("default")))
+#endif
+
+DLL_EXPORT void SharedLibFunc(const std::string& s);
+
+#undef DLL_EXPORT
